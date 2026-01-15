51a5e811146cd1f76a422c4aaaf3f50c92c99732
Maite <maicapref@gmail.com>
2026-01-15 09:56:27 -0300

    edicao

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,15 +2,12 @@
 from fastapi import FastAPI
 
 # Criando uma instância do FastAPI
-# Esta será a base da nossa aplicação
 app = FastAPI()
-
 
 # Criando nossa primeira rota
 # O decorador @app.get("/") diz: "quando alguém acessar a raiz da API, execute esta função"
 @app.get("/")
 def raiz():
-    """Rota raiz - retorna uma mensagem de boas-vindas"""
     return {"mensagem": "Testando inicialização de projeto FastAPI"}
 
 
